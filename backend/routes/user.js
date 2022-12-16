dccae248c6119fe5772d1380287671fb392a5eed
Theo Dubocq <theo.dubocq@gmail.com>
2022-12-16 20:14:45 +0100

    images patch

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
